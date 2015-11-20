fff7caa73d94f49cd35c49d2caf7567e32062f10
chris.hou <chrishou118@gmail.com>
2015-11-20 10:18:56 -0800

    first commit

---
+++ src/main/java/edu/sjsu/food/FoodMiningApplication.java
@@ -0,0 +1,12 @@
+package edu.sjsu.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMiningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodMiningApplication.class, args);
+    }
+}
---
+++ src/main/java/edu/sjsu/food/model/Greeting.java
@@ -0,0 +1,22 @@
+package edu.sjsu.food.model;
+
+/**
+ * Created by yutao on 11/18/15.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
